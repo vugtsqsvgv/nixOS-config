02f64b9a0bcaed5e16d8f92fd9d34771fa2cd2a2
vugtqsvgv <vugtqsvgv@proton.me>
2026-09-25 08:24:28 +0000

    hardware

--- modules/packages.nix
+++ modules/packages.nix
@@ -26,6 +26,7 @@ environment.systemPackages = with pkgs; [
   syncthing
   fuse
   fuse3
+  auto-cpufreq
 ];
 
 fonts.packages = with pkgs; [
--- modules/services.nix
+++ modules/services.nix
@@ -2,6 +2,7 @@
 
 services = {
  openssh.enable = true;
+ auto-cpufreq.enable = true;
  envfs.enable = true;
  displayManager.sddm = {
  enable = true;
