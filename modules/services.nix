f2ccc7579fd490f07c6de305e66ad6b0c1df5fcc
vugtqsvgv <vugtqsvgv@proton.me>
2026-09-25 11:33:11 +0000

    wallpapers

--- modules/services.nix
+++ modules/services.nix
@@ -5,8 +5,8 @@ services = {
  tlp = {
     enable = true;
     settings = {
-        START_CHARGE_THRESH_BATO = 20;
-        STOP_CHARGE_THRESH_BATO = 80;
+        START_CHARGE_THRESH_BAT0 = 20;
+        STOP_CHARGE_THRESH_BAT0 = 80;
       };
   };
  auto-cpufreq.enable = true;
