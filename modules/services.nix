6ea6b02c57cf131a69a6553bc7afdb476bd7f89d
vugtqsvgv <vugtqsvgv@proton.me>
2026-09-25 11:32:11 +0000

    wallpapers

--- modules/services.nix
+++ modules/services.nix
@@ -2,7 +2,13 @@
 
 services = {
  openssh.enable = true;
- tlp.enable = true ;
+ tlp = {
+    enable = true;
+    settings = {
+        START_CHARGE_THRESH_BATO = 20;
+        STOP_CHARGE_THRESH_BATO = 80;
+      };
+  };
  auto-cpufreq.enable = true;
  envfs.enable = true;
  displayManager.sddm = {
